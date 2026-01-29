b8d669ccd25cf0d3a4fc53d5506f6b2fbcb88f5c
babioyunda30-netizen <babioyunda.30@gmail.com>
2026-01-29 12:37:17 +0400

    Add SOURCES constant with sports news RSS feeds

---
+++ sources.js
@@ -0,0 +1,29 @@
+export const SOURCES = [
+  // 🟢 RESMÎ / GÜVENİLİR
+  {
+    name: "BBC Sport",
+    type: "RESMI",
+    lang: "EN",
+    rss: "http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml"
+  },
+  {
+    name: "NTV Spor",
+    type: "RESMI",
+    lang: "TR",
+    rss: "https://www.ntvspor.net/rss"
+  },
+
+  // 🟡 SÖYLENTİ / KOLPA
+  {
+    name: "Fanatik",
+    type: "SOYLENTI",
+    lang: "TR",
+    rss: "https://www.fanatik.com.tr/rss/futbol"
+  },
+  {
+    name: "Fotomaç",
+    type: "SOYLENTI",
+    lang: "TR",
+    rss: "https://www.fotomac.com.tr/rss/anasayfa.xml"
+  }
+];
